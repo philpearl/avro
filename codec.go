235012a8875338b7979fe9c33828397fd22d551b
Phil Pearl <phil.pearl@ravelin.com>
2020-04-29 17:41:49 +0100

    A bit more docs

--- codec.go
+++ codec.go
@@ -1,3 +1,15 @@
+// Package avro is an AVRO decoder aimed principly at decoding AVRO output from
+// Google's Big Query. It decodes directly into Go structs, and uses json tags
+// as naming hints.
+//
+// The primary interface to the package is ReadFile. This reads an AVRO file,
+// combining the schema in the file with type information from the struct passed
+// via the out parameter to decode the records. It then passes an instance of a
+// struct of type out to the callback cb for each record in the file.
+//
+// You can implement custom decoders for your own types and register them via
+// the Register function. github.com/phil/avro/null is an example of custom
+// decoders for the types defined in github.com/unravelin/null
 package avro
 
 import (
