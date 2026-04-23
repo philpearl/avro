7668112ecbfd6ff67fbddf645ec7825ee8aa3b0d
Phil Pearl <phil.pearl@ravelin.com>
2026-04-23 14:16:27 +0100

    Forgot go:fix inline

--- null/null.go
+++ null/null.go
@@ -6,6 +6,8 @@ import "github.com/philpearl/avro/avronull"
 
 // RegisterCodecs registers the codecs from this package and makes them
 // available to avro.
+//
+//go:fix inline
 func RegisterCodecs() {
 	avronull.RegisterCodecs()
 }
